0fc5465b1f436a696335b5c762882ae07aee26be
sxm777-rgb <sxm777@gmail.com>
2026-08-11 10:21:36 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
-const CACHE="rotation-score-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="rotation-score-v2";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./history.js"])))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  if(e.request.mode==="navigate"){
+    e.respondWith(fetch(e.request).then(r=>r.clone().text().then(t=>new Response(t.replace("</body>","<script src=\"./history.js\"></script></body>"),{status:r.status,statusText:r.statusText,headers:r.headers}))).catch(()=>caches.match("./index.html")));
+  }else{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
